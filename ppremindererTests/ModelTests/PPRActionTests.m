574297d7debd6d323e7b081008fa26f7a8825485
David Bernard <davidbernard@optushome.com.au>
2014-10-02 13:22:37 +1000

    PPRActionTests - add tests for dueTimeDescription

---
+++ ppremindererTests/ModelTests/PPRActionTests.m
@@ -0,0 +1,94 @@
+//
+//  PPRActionTests.m
+//  ppreminderer
+//
+//  Created by David Bernard on 2/10/2014.
+//  Copyright (c) 2014 Pegwing Pty Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "PPRAction.h"
+#import "PPRScheduler.h"
+
+@interface PPRActionTests : XCTestCase
+
+@end
+
+@implementation PPRActionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Ensure the the scheudler is initialised
+    [[PPRScheduler sharedInstance]init];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testNotificationDueTimeDescription
+{
+
+    
+    NSDateComponents *dateComponents = [[NSDateComponents alloc]init];
+    dateComponents.hour = 9;
+    dateComponents.minute = 15;
+    
+    PPRScheduleTime *scheduleTime = [[PPRScheduleTime alloc]initWithTimeOfDay:dateComponents
+                                     ];
+    PPRScheduledEvent *scheduledEvent =
+    [[PPRScheduledEvent alloc]initWithEventName:@"TestEventName" scheduledTime:scheduleTime];
+    PPRFacility *facility = [[PPRFacility alloc]init];
+    
+    PPRAction *action = [[PPRAction alloc ]initWithFacility:facility scheduledEvent:scheduledEvent parent:nil actions:nil];
+    action.status = kStatusScheduled;
+    
+    NSDateComponents *dueDateComponents = [[NSDateComponents alloc]init];
+    dueDateComponents.hour = 9;
+    dueDateComponents.minute = 20;
+    action.dueTime = [[NSCalendar currentCalendar] dateFromComponents:dueDateComponents];
+    
+    NSString *dueTimeDescription = [action dueTimeDescription];
+    
+    XCTAssertEqualObjects(dueTimeDescription, @"At 9:15 AM - 9:20:00 AM");
+    
+ 
+}
+
+- (void)testNotificationDueTimeDescriptionWhenCompleted
+{
+    
+    
+    NSDateComponents *dateComponents = [[NSDateComponents alloc]init];
+    dateComponents.hour = 9;
+    dateComponents.minute = 15;
+    
+    PPRScheduleTime *scheduleTime = [[PPRScheduleTime alloc]initWithTimeOfDay:dateComponents
+                                     ];
+    PPRScheduledEvent *scheduledEvent =
+    [[PPRScheduledEvent alloc]initWithEventName:@"TestEventName" scheduledTime:scheduleTime];
+    PPRFacility *facility = [[PPRFacility alloc]init];
+    
+    PPRAction *action = [[PPRAction alloc ]initWithFacility:facility scheduledEvent:scheduledEvent parent:nil actions:nil];
+    action.status = kStatusCompleted;
+    
+    NSDateComponents *dueDateComponents = [[NSDateComponents alloc]init];
+    dueDateComponents.hour = 9;
+    dueDateComponents.minute = 20;
+    action.dueTime = [[NSCalendar currentCalendar] dateFromComponents:dueDateComponents];
+    
+    NSDateComponents *completionDateComponents = [[NSDateComponents alloc]init];
+    completionDateComponents.hour = 9;
+    completionDateComponents.minute = 21;
+    action.completionTime = [[NSCalendar currentCalendar] dateFromComponents:completionDateComponents];
+
+    NSString *dueTimeDescription = [action dueTimeDescription];
+    
+    XCTAssertEqualObjects(dueTimeDescription, @"At 9:15 AM - 9:21:00 AM");
+    
+    
+}
+
+@end
